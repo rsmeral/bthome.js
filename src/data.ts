b2b29b53c7671be70c4580d2eb1ef8dc4cc21ee2
Ron Smeral <ron.smeral@gmail.com>
2022-12-11 17:14:06 +0100

    simpler Int16

--- src/data.ts
+++ src/data.ts
@@ -5,7 +5,7 @@ export const UInt8: NumberType = (val) => [val];
 export const UInt16: NumberType = (val) => [val & 0xff, (val >> 8) & 0xff];
 export const UInt24: NumberType = (val) => [val & 0xff, (val >> 8) & 0xff, (val >> 16) & 0xff];
 export const UInt32: NumberType = (val) => [val & 0xff, (val >> 8) & 0xff, (val >> 16) & 0xff, (val >> 24) & 0xff];
-export const Int16: NumberType = (val) => [...new Uint8Array(new Int16Array([val]).buffer)];
+export const Int16: NumberType = (val) => UInt16(val > 0x7fff ? val - 0x10000 : val);
 
 /**
  * Convert string to ASCII char codes.
